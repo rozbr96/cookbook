a99a69f483b90641bba198729482842ca2e2ac87
Rosemilson <rozbr96@gmail.com>
2023-12-13 10:58:57 -0300

    Display existing categories in categories index page

--- spec/features/user_visits_the_categories_page_spec.rb
+++ spec/features/user_visits_the_categories_page_spec.rb
@@ -9,4 +9,21 @@ feature 'User visit the categories index page' do
 
     expect(current_path).to eq categories_path
   end
+
+  scenario 'and sees existing categories' do
+    user = create :user, role: :admin
+    Category.create! user: user, name: 'Natal'
+    Category.create! user: user, name: 'Páscoa'
+
+    visit categories_path
+
+    expect(page).to have_content 'Natal'
+    expect(page).to have_content 'Páscoa'
+  end
+
+  scenario 'and sees no existing categories' do
+    visit categories_path
+
+    expect(page).to have_content 'Nenhuma Categoria Encontrada'
+  end
 end
